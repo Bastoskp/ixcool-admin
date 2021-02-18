11cc470f7df198c184d3f5d044ec58d40437e754
Kleber Bastos <bastoskp@hotmail.com>
2021-02-17 21:33:49 -0300

    model e Schema

---
+++ models/Turma.model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const turmaSchema = new Schema(
+  {
+    anoEscolar: {
+      type: String,
+      required: [true, "ano escolar obrigatorio"],
+      unique: true,
+    },
+    periodo: String,
+    letraTurma: String,
+    identificacao: String,
+    alunos: [Schema.Types.ObjectId],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = model("Turma", turmaSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
+
+const Turma = require("../models/Turma.model");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  const anoEscolar = "8 ano";
+  const periodo = "matutino";
+  const letraTurma = "A";
+  const identificacao = `${anoEscolar} - ${periodo} - ${letraTurma}`;
+
+  const turmaFalsa = {
+    anoEscolar,
+    periodo,
+    letraTurma,
+    identificacao,
+  };
+  // Turma.create(turmaFalsa).then((turmaDoDb) => {
+  //   console.log(turmaDoDb);
+  //   res.render("index", {
+  //     turmaDoDb,
+  //   });
+  // });
 });
 
 module.exports = router;
